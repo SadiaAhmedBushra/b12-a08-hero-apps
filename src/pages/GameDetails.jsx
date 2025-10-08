6426fffcb53bae05e8968c44a4bd431ab2cd8d79
Sadia Ahmed Bushra <sadiahmedbushra28@gmail.com>
2025-10-09 02:00:54 +0600

    fifth commit: chart

--- src/pages/GameDetails.jsx
+++ src/pages/GameDetails.jsx
@@ -4,6 +4,17 @@ import useGames from "../Hooks/useGames";
 import iconDownloads from "../assets/icon-downloads.png";
 import iconStar from "../assets/icon-ratings.png";
 import iconReview from "../assets/icon-review.png";
+import {
+  BarChart,
+  Bar,
+  ResponsiveContainer,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Rectangle,
+} from "recharts";
 
 const GameDetails = () => {
   const { id } = useParams();
@@ -29,16 +40,23 @@ const GameDetails = () => {
     let updatedList = [];
     const isDuplicate = existingList.some((app) => app.id === game.id);
     if (isDuplicate) {
-        return alert("App is already installed");
+      return alert("App is already installed");
     }
     if (existingList) {
-         updatedList = [...existingList, game];
+      updatedList = [...existingList, game];
+    } else {
+      updatedList.push(game);
     }
-    else {
-        updatedList.push(game);
-    }
-    localStorage.setItem("installedApps", JSON.stringify( updatedList));
+    localStorage.setItem("installedApps", JSON.stringify(updatedList));
   };
+
+
+
+const sortedDataForChart = [...game.ratings].sort(
+  (a, b) => parseInt(b.name) - parseInt(a.name)
+);
+
+
   return (
     <div className="w-11/12 mx-auto">
       <div className="flex flex-col lg:flex-row gap-2 items-center mt-10">
@@ -56,12 +74,12 @@ const GameDetails = () => {
               {game.title}
             </h2>
             <p className="text-[20px] text-gray-500">
-              Developed by
+              <span>Developed by </span>
               <span className="gradient-text">{game.companyName}</span>
             </p>
           </div>
 
-          <hr className="w-full border-t border-gray-200 mt-2" />
+          <hr className="w-full border-t border-gray-200 my-2" />
 
           <div className="flex flex-col lg:flex-row justify-between items-center lg:items-start gap-2 ">
             <div className="flex flex-col items-center gap-2 mb-4">
@@ -89,15 +107,40 @@ const GameDetails = () => {
             </div>
           </div>
 
-          <button onClick={handleInstalledApps} className="btn bg-[#00d390] text-white">Install Now</button>
+          <button
+            onClick={handleInstalledApps}
+            className="btn bg-[#00d390] text-white"
+          >
+            Install Now
+          </button>
         </div>
       </div>
       <hr className="w-full border-t border-gray-200 mt-5" />
 
-      <h2 className="text-[26px] text-[#001931] font-bold mt-5">Ratings</h2>
+      {/* chart */}
+      <div className="">
+        <h2 className="text-[26px] text-[#001931] font-bold mt-5">Ratings</h2>
+        <div className="bg-base-100 rounded-xl p-4 h-80">
+    
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart layout="vertical" data={sortedDataForChart}>
+            
+              <XAxis type="number" tick={{ fontSize: 16 }} />
+              <YAxis
+                type="category"
+                dataKey="name"
+                tick={{ fontSize: 16 }}
+              />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="count" fill="#ff8811" />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
 
       <h2 className="text-[26px] text-[#001931] font-bold mt-5">Description</h2>
-      <p className="text-[20px] text-gray-500">{game.description}</p>
+      <p className="text-[20px] text-gray-500 mb-10">{game.description}</p>
     </div>
   );
 };
